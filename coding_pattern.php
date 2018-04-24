d43d3282c961a092474975b0737bf97315596147
soft-smart <softsmart2018@gmail.com>
2018-04-24 12:41:38 +0530

    change file name

---
+++ coding_pattern.php
@@ -0,0 +1,24 @@
+<?php
+
+//--------------------------------------    Coding Structure Block ---------------------------------- //
+
+/* All classes will be in camel case 
+
+	ex: class MyClass
+*/
+
+
+/* All method name will be defined as
+	
+    ex: public function getNameClassData(){ }
+*/
+
+
+ /* All valiable name will be defined as
+	
+    ex: $name or $name_data   ( all small and meaning full name )
+*/
+
+ 
+
+?>
